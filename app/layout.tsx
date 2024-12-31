ae8787c5281530409610ba21d4e86d7c1b10b6c7
marc <91703789+marcgbmh@users.noreply.github.com>
2024-12-31 01:19:26 +0100

    chore: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,33 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Jex",
-  description: "Collect Alomst Objects",
+  metadataBase: new URL("https://mint.jex.us"),
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Jex",
+    url: "https://mint.jex.us",
+    siteName: "Jex",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jex",
+    description: "Library.",
+    images: ["/og.png"],
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
